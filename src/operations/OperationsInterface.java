b5d39c2715d2428c268efccb498eb9b2fc3ed38f
franciscog852 <frankie.j.gonzaleziii@gmail.com>
2023-12-06 16:30:23 -0700

    Interface

--- src/operations/OperationsInterface.java
+++ src/operations/OperationsInterface.java
@@ -1,3 +1,8 @@
+/**
+ * @author Francisco Gonzalez
+ * Interface for all operations that will force them to have a way to open their menus
+ */
+
 package operations;
 
 public interface OperationsInterface {
